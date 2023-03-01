78bf7ecb31054f44a19451eb6f4648bebe8f70b4
Jon Seijas <jseijasco22dw@ikzubirimanteo.com>
2023-03-01 23:26:27 +0100

    Lanzamiento de pelota alpha

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+/* Modo oscuro-claro */
+
 function theme() {
 	if (document.getElementById("body").className == "bodylight") {
 		document.getElementById("body").className = "bodydark";
@@ -19,3 +21,51 @@ function theme() {
 		document.getElementById("aa2").className = "article a2 articlelight";
 	}
 }
+
+/* Fin del modo oscuro-claro */
+
+/* Boton de lanzar */
+
+var power=0;
+    var time;
+    var tirado=false;
+    
+    function start(){
+		let finalPower;
+        if(!tirado){
+            time = setInterval(segundos, 10);
+            tirado = true;
+        } else {
+            movimiento();
+        }
+        
+    }
+
+	function movimiento(){
+		tirado = false;
+            console.log(power);
+			document.documentElement.style.setProperty('--midY',-power*0.2 + "vw");
+			document.documentElement.style.setProperty('--midX',-power*0.3 + "vw");
+			document.documentElement.style.setProperty('--finalY',-power*0.13 + "vw");
+			document.documentElement.style.setProperty('--finalX',-power*0.7 + "vw");
+            clearInterval(time);
+            power=0;
+	}
+
+    function fixStart(){
+		let finalPower;
+        if(tirado){
+            movimiento();
+			
+        }
+    }
+    
+    function segundos(){
+        power++;
+        if (power > 100){
+            power=0;
+        }
+        document.querySelector('#fuerza').value=power;
+    }
+
+/* Fin del boton de lanzar */
